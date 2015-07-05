33e2be9da3089355489dc4427ad81b1ef0dc4e58
khetanrajesh <erajeshkhetan@gmail.com>
2015-07-06 00:01:42 +0530

    updated

--- performAction.js
+++ performAction.js
@@ -1,101 +1,99 @@
-function Perform(action,display,ele){
-            
-          var _this =this;
-    
-         if(action=="start" ){
-             
-             
-            this.ms= this.ms+1;
-            
-            if(this.ms == 1000) {
-                this.ms = 0;
-                this.sec=this.sec+1;
-            }
-             
-            if(this.sec == 60 ) {
-                this.sec=0;
-                this.min=this.min+1;
-            }
-            
-            if(this.min == 60) {
-                this.min=0;
-                this.hr=this.hr+1;
-            }
-            
-           display.innerHTML = this.hr + ":" + this.min + ":" + this.sec + ":" + this.ms;
-           this.timeout = setTimeout(function(){_this.perform(action,display) ;} ,1);  
-             
-            //ele.nextElementSibling.nextElementSibling.nextElementSibling.disabled=false;
-             
-            
+function splitTime(hr, min, sec, ms) {
+
+    this.hr = hr;
+    this.min = min;
+    this.sec = sec;
+    this.ms = ms;
+
+}
+
+//perform tasks based on the action "start"/"stop"/"reset"/'split"
+function perform(action, display, ele) {
+
+    var _this = this;
+
+    if (action == "start") {
+
+        this.ms = this.ms + 1;
+
+        if (this.ms == 1000) {
+            this.ms = 0;
+            this.sec = this.sec + 1;
         }
-        
-        if(action=="stop" ){
-          
-            clearTimeout(this.timeout);
-            
-            display.nextElementSibling.lastElementChild.style.padding="10px 15px";
-                
-            if(!this.lastSplitAdded) {
 
-                this.history.push(new splitTime(this.hr ,this.min ,this.sec ,this.ms));
-                this.lastSplitAdded=1;
-            }
-
-            var jsonString = JSON.stringify(this.history);
-            obj = JSON.parse(jsonString);
-
-            var summary="";
-
-
-            for( var time in obj){
-
-                summary = summary + obj[time].hr + ":" + obj[time].min + ":" + obj[time].sec + ":" + obj[time].ms + "<br>" ;
-                
-                
-            }
-            
-            display.nextElementSibling.lastElementChild.innerHTML =summary;
-            
-
+        if (this.sec == 60) {
+            this.sec = 0;
+            this.min = this.min + 1;
         }
-        
-        if(action=="reset" ) {
-            
-            this.hr=0;
-            this.hr=0;
-            this.min=0;
-            this.sec=0;
-            this.ms=0; 
-            clearTimeout(this.timeout);
-            this.history.length=0;
-            this.lastSplitAdded=false;
-            display.innerHTML = "00:00:00:00";
-            this.summary="";
-            
-            display.nextElementSibling.lastElementChild.innerHTML ="";
-            
-            
-            display.nextElementSibling.lastElementChild.style.padding="";
-            }
-    
 
-            
-        if(action=="split"){
-            
-            
-            
-                this.history.push(new splitTime(this.hr ,this.min ,this.sec ,this.ms));
+        if (this.min == 60) {
+            this.min = 0;
+            this.hr = this.hr + 1;
+        }
 
-                this.summary = this.summary + this.hr + ":" + this.min + ":" + this.sec + ":" + this.ms + "<br>" ;
-                    
-                display.nextElementSibling.lastElementChild.style.padding="10px 15px";
-                
-                display.nextElementSibling.lastElementChild.innerHTML =this.summary;
-            
-            
-                
-    
-         }
+        display.innerHTML = this.hr + ":" + this.min + ":" + this.sec + ":" + this.ms;
+        this.timeout = setTimeout(function() {
+            _this.perform(action, display);
+        }, 1);
 
     }
+
+    if (action == "stop") {
+
+        clearTimeout(this.timeout);
+
+        display.nextElementSibling.lastElementChild.style.padding = "10px 15px";
+
+        if (!this.lastSplitAdded) {
+
+            this.history.push(new splitTime(this.hr, this.min, this.sec, this.ms));
+            this.lastSplitAdded = 1;
+        }
+
+        var jsonString = JSON.stringify(this.history);
+        obj = JSON.parse(jsonString);
+
+        var summary = "";
+
+
+        for (var time in obj) {
+
+            summary = summary + obj[time].hr + ":" + obj[time].min + ":" + obj[time].sec + ":" + obj[time].ms + "<br>";
+
+        }
+
+        display.nextElementSibling.lastElementChild.innerHTML = summary;
+
+    }
+
+    if (action == "reset") {
+
+        this.hr = 0;
+        this.hr = 0;
+        this.min = 0;
+        this.sec = 0;
+        this.ms = 0;
+        clearTimeout(this.timeout);
+        this.history.length = 0;
+        this.lastSplitAdded = false;
+        display.innerHTML = "00:00:00:00";
+        this.summary = "";
+        display.nextElementSibling.lastElementChild.innerHTML = "";
+        display.nextElementSibling.lastElementChild.style.padding = "";
+    }
+
+
+
+    if (action == "split") {
+
+        this.history.push(new splitTime(this.hr, this.min, this.sec, this.ms));
+
+        this.summary = this.summary + this.hr + ":" + this.min + ":" + this.sec + ":" + this.ms + "<br>";
+
+        display.nextElementSibling.lastElementChild.style.padding = "10px 15px";
+
+        display.nextElementSibling.lastElementChild.innerHTML = this.summary;
+
+    }
+
+}
--- sw.js
+++ sw.js
@@ -1,31 +1,75 @@
-        var i=0;
-        var arraySW = new Array();
-
-        function splitTime(hr,min,sec,ms){
-            
-        this.hr=hr;
-        this.min=min;
-        this.sec=sec;
-        this.ms=ms; 
-            
-        }
+     
+    var i=0;
+    var arraySW = new Array();
+    
+    function controller(){
         
-        function sw(){
-        this.hr=0;
-        this.min=0;
-        this.sec=0;
-        this.ms=0; 
-        this.perform= Perform;
-        this.timeout=null;
-        this.history = new Array();  
-        this.lastSplitAdded=0;
+        document.getElementById("body").addEventListener('click',function(){
             
-        this.summary="";
-        }
+            if (event.target.id == 'start' || event.target.id == 'stop' || event.target.id == 'reset' ) {
+                disabler(event.target);
+                execute(event.target);
+            }
+            if(event.target.id == 'split'){
+                execute(event.target);  
+            }
+        });
+ 
+     }
+    
+    //Function to disable the buttons based on click of other buttons
+    function disabler(target){
 
-        function newSW(){
+            console.log("disabler started");
+            console.log(target.id )
+    
+            if(target.id == "start"){
+                target.parentElement.lastElementChild.disabled=false;
+                target.nextElementSibling.disabled=false;    
+            }
             
-             
+            if(target.id == "stop"){
+                target.previousElementSibling.disabled=true ;     
+                target.nextElementSibling.nextElementSibling.disabled=true;
+            }  
+            if(target.id == "reset"){
+                target.previousElementSibling.previousElementSibling.disabled=false ;      
+                target.previousElementSibling.disabled=true;
+                target.nextElementSibling.disabled=true;    
+            }
+        
+    }
+    
+    //this function calls perform based on the action
+    function execute(target){
+            
+            action=target.id;
+            var stopWatchId=target.parentNode.parentNode.id ;
+            var display= document.getElementById(stopWatchId).firstElementChild ; //the display box of the stop-watch
+            for(var j=0;j<arraySW.length;j++){
+                if(j==stopWatchId){  
+                arraySW[j].perform(action,display,this);
+                }
+            }
+    }
+
+    //the stopWatch Object
+    function sw(){
+            this.hr=0;
+            this.min=0;
+            this.sec=0;
+            this.ms=0;
+            //perform method perform the task of start,stop,reset,split
+            this.perform= perform;
+            this.timeout=null;
+            this.history = new Array();  
+            this.lastSplitAdded=0;
+            this.summary="";
+    }
+        
+    //create a new Stop Watch and Dispaly it on Screen
+    function newSW(){
+
             arraySW[i] = new sw();
             var  div = document.getElementById("template");
             var divClone = div.cloneNode(true);
@@ -34,62 +78,13 @@
             document.body.appendChild(divClone);
             i++;
             
-        }
+    }
         
         
-        function execute(ele,activity){
-
-           var divId=ele.parentNode.parentNode.id ;
-            
-           var display= document.getElementById(divId).firstElementChild ;
-
-           for(var j=0;j<arraySW.length;j++){
-                
-                if(j==divId){  
-                arraySW[j].perform(activity , display ,ele);
-                
-                }
-            
-            }
-            
-        }
         
 
-    
-        function disabler(ele,action){
-            
-            
-            if(action == "start"){
-            
-             ele.parentElement.lastElementChild.disabled=false;
-             
-            ele.nextElementSibling.disabled=false;
-                
-                
-            }
-            
-            
-            if(action == "stop"){
-            
-            ele.previousElementSibling.disabled=true ;
-            
-            ele.nextElementSibling.nextElementSibling.disabled=true;
-
-                
-            }
-            
-            
-            if(action == "reset"){
-            
-            ele.previousElementSibling.previousElementSibling.disabled=false ;      
-            ele.previousElementSibling.disabled=true;
-            ele.nextElementSibling.disabled=true;
-            
-                
-            }
+       
         
-        
-        }
 
 
 
